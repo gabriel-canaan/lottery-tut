b40e7461064c4d951c43a010be72ea09468cc257
gabriel <777canaan@gmail.com>
2018-01-21 18:58:45 +1300

    add pickwinner restricted test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -48,7 +48,6 @@ describe('Lottery Contract', () => {
       value: web3.utils.toWei('0.02', 'ether')
     });
     const players = await lottery.methods.getPlayers().call({from: accounts[0]});
-
     assert.equal(accounts[0], players[0]);
     assert.equal(accounts[1], players[1]);
     assert.equal(accounts[2], players[2]);
@@ -71,5 +70,16 @@ describe('Lottery Contract', () => {
     } catch(err) {
       assert(err);
     }
+  });
+  it('sends money to the winner and resets the players array' async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+    const initialBalance = await web3.eth.getBalance()accounts[0];
+    await lottery.methods.pickWinner().send({from: accounts[0]});
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
   });
 });
